c744a2b4434ee935d6d6d1f6a11b35e0f5b4b98d
kord1011 <48543952+kord1011@users.noreply.github.com>
2019-04-05 11:19:14 +0530

    initial commit

---
+++ p.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, r;
+
+    printf("Enter number of r: ");
+    scanf("%d",&r);
+
+    for(i=1; i<=r; ++i)
+    {
+        for(j=1; j<=i; ++j)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
